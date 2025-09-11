d55e2394f4797c2d8f67989e51f81e975163c1ce
sm praneeth kumar <smparneethkumr431gmail.com>
2025-09-11 13:10:42 +0530

    this is my first commit

---
+++ src/com/dsa/loops/loops.java
@@ -0,0 +1,38 @@
+package com.dsa.loops;
+
+import java.util.Scanner;
+
+public class loops {
+	
+	public static void main (String[] args) {
+		System.out.println("this is my first line for DSA in JAVA");
+		
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+
+        boolean isLeapYear = false;
+
+        // Leap year conditions:
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                // Century year must be divisible by 400
+                if (year % 400 == 0) {
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+
+        if (isLeapYear) {
+            System.out.println(year + " is a Leap Year.");
+        } else {
+            System.out.println(year + " is NOT a Leap Year.");
+        }
+
+        scanner.close();
+
+	}
+}
