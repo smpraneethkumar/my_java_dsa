056a8bfc7823f384055a97cc48feece091821f98
sm praneeth kumar <smparneethkumr431gmail.com>
2025-09-13 09:11:30 +0530

    arrays

--- src/com/dsa/day_3/loops.java
+++ src/com/dsa/day_3/loops.java
@@ -1,4 +1,4 @@
-package com.dsa.loops;
+package com.dsa.day_3;
 
 import java.util.Scanner;
 
---
+++ src/com/dsa/day_4/Print_primes.java
@@ -0,0 +1,35 @@
+package com.dsa.day_4;
+
+public class Print_primes {
+
+	public static void main(String[] args) {
+	
+		
+//		CHART GPT
+		
+		for (int num = 2; num <= 100; num++) {
+            boolean isPrime = true;
+
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(num + " ");
+            }
+        }
+		
+		int a =10;
+		
+		
+		{
+			a= 100;
+			System.out.println(a);
+		}
+		System.out.println(a);
+	}
+
+}
